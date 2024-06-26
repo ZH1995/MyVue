776ef7b84713b6b3737783575e8f3da8a4c39496
zh1995 <rwzsjfm@qq.com>
2024-06-26 23:34:05 +0800

    add record page

--- README.md
+++ README.md
@@ -22,3 +22,31 @@ npm run lint
 
 ### Customize configuration
 See [Configuration Reference](https://cli.vuejs.org/config/).
+
+### Url
+
+- http://localhost:8080/feed-record —— 访问喂食记录页面
+
+- http://localhost:8080/sleep-record —— 访问睡眠记录页面
+
+### Check port status
+sudo lsof -i :[port]
+
+### Structure
+```
+/src
+	/components：存放 Vue 组件
+		FeedForm.vue：喂食记录组件
+		SleepForm.vue：睡眠记录组件
+		ChartData.vue：图表显示组件
+	/views：存放视图
+		Home.vue：主页视图，可能会包含以上的表单和图表组件
+		FeedRecord.vue: 喂食记录页面
+		SleepRecord.vue: 睡眠记录页面
+	App.vue：主应用文件，整合各个视图和组件
+	main.js：Vue 应用的入口文件
+	/router
+		index.js：定义路由，连接不同的视图
+	/public
+	index.html：项目的HTML入口文件
+```
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,29 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import Home from '@/views/HomeView.vue';
+import FeedRecord from '../views/FeedRecord.vue';
+import SleepRecord from '../views/SleepRecord.vue';
 
-const router = createRouter({
-  history: createWebHistory(),
-  routes: [
-    {
+const routes = [
+  {
+    path: '/feed-record',
+    name: 'FeedRecord',
+    component: FeedRecord
+  },
+  {
+    path: '/sleep-record',
+    name: 'SleepRecord',
+    component: SleepRecord
+  },
+  {
       path: '/',
       name: 'home',
       component: Home
     },
-  ],
+];
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
 });
 
 export default router;
