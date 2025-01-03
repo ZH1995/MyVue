bf340b44a07970bd59b307011ce3473ea6f4595c
zh1995 <rwzsjfm@qq.com>
2025-01-04 06:57:18 +0800

    transmit paramter from child component to parent component

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,23 @@
 <template>
   <div>
-    姓名：<input ref="name" abc="123"/><br/>
-    <button v-on:click="showRes">提交</button>
+    <MySalaryInfo ref="salaryInfo"></MySalaryInfo>
+    <button @click="showResult">查看薪水</button>
   </div>  
 </template>
 
 <script setup lang="ts">
 import {ref} from 'vue'
-let name = ref()
-  function showRes(){
-    console.log(name)
-    console.log(name.value)
-    console.log(name.value.value)
-    console.log(name.value.getAttribute('abc'))
-  }
+import MySalaryInfo from './components/MySalaryInfo.vue'
+let salaryInfo = ref()
+
+function showResult(){
+  console.log(salaryInfo)
+  console.log(salaryInfo.value)
+  console.log(salaryInfo.value.userName)
+  console.log(salaryInfo.value.salary)
+}
 </script>
 
 <style scoped>
-  .userInfo{
-    background-color: cyan;
-    widows: 80%;
-  }
-  .userInfo span{
-    background-color: coral;
-    margin-left: 10px;
-    border: 1px;
-    border-radius: 5px;
-  }
+ 
 </style>
---
+++ src/components/MySalaryInfo.vue
@@ -0,0 +1,20 @@
+<template>
+    姓名：<input v-model="userName"/><br/>
+    薪水：<input type="number" v-model="salary"/><br/>
+    
+</template>
+
+<script lang="ts">
+    export default {
+        name:'MySalaryInfo'   
+    }
+</script>
+
+<script setup lang="ts">
+import {ref} from 'vue'
+let userName = ref('zhangsan')
+let salary = ref(1000)
+defineExpose({userName,salary})
+</script>
+
+<style></style>
